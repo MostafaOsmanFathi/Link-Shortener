9ada3914bd38bc011fdea0f9c2b16e3782963f71
mostafa <mostafa.osman.fathi@gmail.com>
2025-06-27 15:57:48 +0300

    adding loggers

--- src/main/java/dev/mostafa/Link_Shortener/ShortLinkController.java
+++ src/main/java/dev/mostafa/Link_Shortener/ShortLinkController.java
@@ -3,39 +3,50 @@ package dev.mostafa.Link_Shortener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("links")
 public class ShortLinkController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ShortLinkController.class);
+
     @Autowired
     ShortLinkService shortLinkService;
 
     @GetMapping({"", "/"})
     public List<ShortLink> index() {
+        logger.info("Fetching all short links");
         return shortLinkService.getShortLinks();
     }
 
     @PostMapping("/")
     public ShortLink createShortLink(@RequestBody String originalUrl) {
-        return shortLinkService.createShortLink(originalUrl.substring(1, originalUrl.length() - 1));
+        String cleanUrl = originalUrl.substring(1, originalUrl.length() - 1);
+        logger.info("Creating short link for: {}", cleanUrl);
+        return shortLinkService.createShortLink(cleanUrl);
     }
 
     @GetMapping("/{id}")
     public ShortLink getById(@PathVariable int id) {
+        logger.info("Fetching short link by ID: {}", id);
         return shortLinkService.getById(id);
     }
 
     @GetMapping("/resolve/{code}")
     public RedirectView resolveLinkByCode(@PathVariable String code) {
-        String OriginalUrl = shortLinkService.getByShortCode(code).getOriginalUrl();
-        System.out.println(OriginalUrl);
-        return new RedirectView(OriginalUrl);
+        logger.info("Resolving short link with code: {}", code);
+        String originalUrl = shortLinkService.getByShortCode(code).getOriginalUrl();
+        logger.info("Redirecting to: {}", originalUrl);
+        return new RedirectView(originalUrl);
     }
 
     @DeleteMapping("/{code}")
     public boolean deleteShortLink(@PathVariable String code) {
+        logger.info("Deleting short link with code: {}", code);
         return shortLinkService.deleteShortLinkByCode(code);
     }
 }
